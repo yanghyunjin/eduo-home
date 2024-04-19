8433f72486c740ecbdf012bb73a8b43417ceb6f2
hyunjinyang <yang.skkuice@gmail.com>
2024-04-19 16:55:18 +0900

    Update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
-  description: "This is Home for Startup Nextjs Template",
+  title: "EDUO - LEARNING",
+  description: "EDUO - LEARNING",
   // other metadata
 };
 
